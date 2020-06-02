4a589c601e0c73a2dbe0a3d3c46c9305ac621850
ini1990 <ini1990g@gmail.com>
2020-06-02 14:59:09 +0300

    style

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -8,11 +8,11 @@ function run()
 {
     $description = 'What number is missing in the progression?';
     $questions = [];
-    $sizeProgression = 10;
+    $progressionSize = 10;
     for ($i = 0; $i < ROUNDS_COUNT; $i++) {
         $start = rand(1, 20);
         $difference = rand(2, 10);
-        $progression = getProgression($start, $difference, $sizeProgression);
+        $progression = getProgression($start, $difference, $progressionSize);
         $index = array_rand($progression);
         $correctAnswer = $progression[$index];
         $progression[$index] = '..';
@@ -22,9 +22,9 @@ function run()
     \BrainGames\play($description, $questions);
 }
 
-function getProgression($start, $difference, $sizeProgression)
+function getProgression($start, $difference, $progressionSize)
 {
-    $stepsAmount = $sizeProgression - 1;
+    $stepsAmount = $progressionSize - 1;
     $end = $start + ($difference * $stepsAmount);
     return range($start, $end, $difference);
 }
